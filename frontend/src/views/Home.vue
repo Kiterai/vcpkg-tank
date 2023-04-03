3b22358dd801a1ae9e2e51a0c29e5f2cb2074280
kiterai <kiterai@foolslab.net>
2023-04-03 19:57:35 +0900

    split

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -49,7 +49,7 @@ export default defineComponent({
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          pkgs: [target_pkglist],
+          pkgs: target_pkglist.split(' '),
         }),
       })
         .then((res) => {
